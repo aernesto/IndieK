a9fd51d680414ac3e1cbba610e9f545e4526a0e2
Adrian Radillo <adrian.radillo@gmail.com>
2019-05-29 21:27:22 -0400

    logic for topics creation works
    right now, use create_topic function to create new topic
    the topic won't be created if one with the same name
    already exists in the database.
    
    functions list_topics and get_topic_by_name also programed

--- python_scripts/simple_attempts.py
+++ python_scripts/simple_attempts.py
@@ -14,8 +14,10 @@ import json
 
 PATH_TO_CONFIG = '/home/adrian/.ikconfig'
 
+# keys are what I use in the code, values are what is used in the database
 COLL_NAMES = {'topics': 'topics',
               'subtopic_links': 'subtopic_links'}
+TOPIC_FIELDS = {'name': 'name', 'description': 'description'} 
 
 def ik_connect():
     with open(PATH_TO_CONFIG) as f:
@@ -24,12 +26,13 @@ def ik_connect():
     db_name = conf['database']
 
     # check appropriate db and collections exist; offer to create them if they don't
+    # note that creation might fail if proper permissions aren't set for the user
     if not conn.hasDatabase(db_name):
         print(f"database {db_name} not found")
         ans = input("would you like to create it? (y + ENTER for yes) ")
         if ans == 'y':
             db.createDatabase(name=db_name)
-        print(f"database {db_name} created")
+            print(f"database {db_name} created")
 
     db = conn[db_name]
 
@@ -38,18 +41,76 @@ def ik_connect():
         ans = input("would you like to create it? (y + ENTER for yes) ")
         if ans == 'y':
             db.createCollection(name=COLL_NAMES['topics'], className='Collection')
-        print(f"collection {COLL_NAMES['topics']} created")
+            print(f"collection {COLL_NAMES['topics']} created")
+
+#    db[COLL_NAMES['topics']].ensureFulltextIndex(list(TOPIC_FIELDS.values())) # somehow this fails
 
     if not db.hasCollection(COLL_NAMES['subtopic_links']):
         print(f"collection {COLL_NAMES['subtopic_links']} not found in db {db.name}")
         ans = input("would you like to create it? (y + ENTER for yes) ")
         if ans == 'y':
             db.createCollection(name=COLL_NAMES['subtopic_links'], className='Edges')
-        print(f"collection {COLL_NAMES['subtopic_links']} created")
+            print(f"collection {COLL_NAMES['subtopic_links']} created")
 
     return db
 
+def get_topic_by_name(db, topic_name):
+    """
+    fetches topic if exists, false otherwise
+
+    todo: figure out what batchSize does
+
+    Args:
+        db: DBHandle
+        topic_name: string, name of topic
+
+    Returns:
+        pyArango.query.SimpleQuery
+    """
+    return db[COLL_NAMES['topics']].fetchByExample({TOPIC_FIELDS['name']: topic_name}, batchSize=100)
+    
+def list_topics(db):
+    return db[COLL_NAMES['topics']].fetchAll()
+
+
+def create_topic(db, name, descr):
+    if get_topic_by_name(db, name):
+        print(f"topic '{name}' already exists")
+    else:
+        doc = db[COLL_NAMES['topics']].createDocument({
+            TOPIC_FIELDS['name']: name,
+            TOPIC_FIELDS['description']: descr})
+        doc.save()
 
 
 if __name__ == "__main__":
     db = ik_connect()
+    print('list topics')
+    print(list_topics(db))
+
+    print('checking emptyness in if statement')
+    t = get_topic_by_name(db, 'salut')
+    if t:
+        print('topic found!')
+    else:
+        print('topic not found!')
+
+    print('creating topic "salut"')
+    create_topic(db, 'salut', 'premier topic')
+    
+    print('list topics')
+    print(list_topics(db))
+
+    print('checking emptyness in if statement')
+    t = get_topic_by_name(db, 'salut')
+    if t:
+        print('topic found!')
+    else:
+        print('topic not found!')
+
+    print('trying to re-create same topic')
+    create_topic(db, 'salut', 'premier topic')
+    
+    print('list topics')
+    print(list_topics(db))
+
